c1654175f2c6d44cf2ac3ea449aae4a273622de5
Brandon <brandon@willjasen.com>
2021-01-04 13:36:37 -0500

    Capture from Aaron McCray

---
+++ CleanUpUsers.ps1
@@ -0,0 +1,62 @@
+<#
+Requirements:
+Active Directory module or run from DC
+
+#>
+
+#User Variables: 
+$FileLocation = Get-Location
+$OutputFileLocation = "C:\Scripts"
+$OutputFileName = "\test.csv" # Backslash is needed before file name in this variable
+[INT]$OverThisManyDays = 90
+$TargetOU = "" #This has to be the OUs distinguished name
+
+
+#Script Variables:
+$OutputFileLocationPlusName = $OutputFileLocation + $OutputFileName
+$TodaysDate = (Get-Date)
+$OldDate = $TodaysDate.AddDays(-($OverThisManyDays))
+$Timespan = "$OverThisManyDays" + ":00:00:00"
+$EnabledPCs = Search-ADAccount -ComputersOnly -AccountInactive -TimeSpan $Timespan
+
+ 
+##Used for testing on a single system.Replace Investigations with any PC name and remove the ## in front of the next line. That will enable targeting only that system.
+##$EnabledPCs = $EnabledPCs | ?{$_.Name -eq "Investigations"}
+##
+$TooLongSinceLogon = $EnabledPCs | ?{$_.LastLogonDate -lt $OldDate} 
+
+
+#Main Body:
+Try
+    {
+        If((Test-Path -Path $OutputFileLocation) -eq $false)
+            {
+                Write-Host "Was Borked. Shouldn't be now. Run it again." -ForegroundColor Yellow
+                New-Item -Path $OutputFileLocation -ItemType Directory
+            }
+        else #It's Working
+            {
+                Write-host "Not Borked. Check the CSV when complete." -ForegroundColor Green
+                $TooLongSinceLogon |Select-Object Name, DistinguishedName, LastLogonDate| Sort-Object LastLogonDate| Export-Csv -Path $OutputFileLocationPlusName -Force -NoTypeInformation
+                    #Be real careful here:
+                    #Delete the # at the end of the next line if you want to disable the systems found.
+                    <#
+                    ForEach ($oldsystem in $TooLongSinceLogon)
+                    {
+                        Write-host ""$oldsystem" is being disabled" -ForegroundColor Yellow
+                        $UserDN = (Get-ADUser -Identity $_.SamAccountName).distinguishedName
+                        Get-ADComputer "$oldsystem"| Set-ADComputer -Enabled $False
+                        Move-ADObject -Identity $UserDN -TargetPath $TargetOU -WhatIf
+                        Write-Host ""$oldsystem" is disabled" -ForegroundColor Green
+                    }
+                    #>
+                    ##Stop being careful here:
+            }
+    }
+    Catch #If it all goes haywire
+        {
+             Write-Host "Suuuper Borked Yo. Call Stephen." -ForegroundColor Red
+             
+        }
+ 
+
